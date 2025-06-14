4fd60a5646b5a0b6a35cee0e7db50e49a79b97a7
Yusnegg <yusnelleyvag@gmail.com>
2025-06-13 23:21:17 -0400

    💡: Refactor file drop handling to start server with selected folder path

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import http.server
+import socketserver
+import threading
 from tkinter import *
+import os
 from tkinterdnd2 import DND_FILES, TkinterDnD
 
 PORT = 8000
+server_thread = None
+httpd = None
 
+def start_server(folder_path):
+    global server_thread, httpd
 
-def start_server():
-    print("Server run port {PORT}")
+    os.chdir(folder_path)
+
+    handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), handler)
+
+    def server_loop():
+        print(f"Server run port {PORT}")
+        httpd.serve_forever()
+
+    server_thread = threading.Thread(target=server_loop, daemon=True)
+    server_thread.start()
 
 def on_drop(event):
+    folder = event.data.strip("{}")
+    if not folder:
+        info_label.config(text="No files dropped")
+        return
+    else:
+        info_label.config(text="Files dropped")
+        start_server(folder)
+
     print("📂 Dropped files:", event.data)
 
 root = TkinterDnD.Tk()
 root.title("Simple HFS")
-root.geometry("350x400")
+root.geometry("350x300")
 root.resizable(False, False)
 
 label = Label(root, text="Drop files here", bg="lightgray", width=40, height=10)
